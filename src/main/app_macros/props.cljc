9241adf8f789790051486112df03d61e3210d46d
Jannis Pohlmann <contact@jannispohlmann.de>
2016-02-22 14:59:04 +0100

    Move properties spec parsing into its own namespace with tests
    This will allow us to re-use the properties spec parser e.g. in
    custom implementations of (query ...).

---
+++ src/main/app_macros/props.cljc
@@ -0,0 +1,103 @@
+(ns app-macros.props)
+
+(defn pad-by
+  "Add pad in between any two consecutive values in coll for which
+   pred returns the same result. As an example, assume the following
+   use:
+
+       (pad-by type :same-type [:foo :bar [1 2] {3 4} {5 6}])
+
+   The result would be
+
+       [:foo :same-type :bar [1 2] {3 4} :same-type {5 6}].
+
+   If called without coll, returns a transducer."
+  ([pred pad]
+   (fn [rf]
+     (let [pv (volatile! nil)]
+       (fn
+         ([] (rf))
+         ([result] (rf result))
+         ([result input]
+          (let [prior @pv]
+            (vreset! pv input)
+            (if (pred prior input)
+              (rf (rf result pad) input)
+              (rf result input))))))))
+  ([pred pad coll] (sequence (pad-by pred pad) coll)))
+
+(defn value?
+  "Returns true if x is considered a basic EDN value."
+  [x]
+  (or (number? x) (string? x) (keyword? x) (= x '_)))
+
+(def property-types
+  "Property types supported by the properties parser."
+  [{:type :property
+    :test #(symbol? %)
+    :name #(name %)}
+   {:type :link
+    :test #(and (vector? %)
+                (= 2 (count %))
+                (value? (second %)))
+    :name #(first %)}
+   {:type :join
+    :test #(and (map? %)
+                (= 1 (count %)))
+    :name #(first (keys %))}])
+
+(defn- property-resolve
+  "Given a property prop, resolves the field :type or :name into
+   the corresponding property type or name."
+  [prop field]
+  (let [match (first (filter #((:test %) prop) property-types))]
+    (if (fn? (get match field))
+      ((get match field) prop)
+      (get match field))))
+
+(defn property-type
+  "Returns the property type for prop."
+  [prop]
+  (property-resolve prop :type))
+
+(defn property-name
+  "Returns a property name for prop. If passed a parent != nil,
+   the property name is namespaced according to the parent property
+   name."
+  [parent prop]
+  (symbol (some-> parent (property-resolve :name) name)
+          (some-> prop (property-resolve :name) name)))
+
+(defn parse
+  "Parses a properties specification like
+   [user [name email {friends User}] [current-user _]] into
+   a flat collection with the following structure:
+
+   [{:name user/name :type :property}
+    {:name user/email :type :property}
+    {:name user/friends :type :join :target User}
+    {:name current-user :type :link :target _}].
+
+   From this it is trivial to generate keys for destructuring
+   view props and an Om Next query."
+  [spec]
+  (letfn [(parse-prop [parent p]
+            (let [name (property-name parent p)
+                  type (property-type p)]
+              (case type
+                :property {:name name :type type}
+                :link     {:name name :type type
+                           :target (second p)}
+                :join     {:name name :type type
+                           :target (first (vals p))}
+                :else     nil)))
+          (parse-step [result [p children]]
+            (concat result
+                    (cond
+                      (nil? children)    [(parse-prop nil p)]
+                      (vector? children) (mapv #(parse-prop p %)
+                                               children))))]
+    (->> (pad-by #(= (property-type %1) (property-type %2)) nil spec)
+         (partition-all 2 2)
+         (reduce parse-step [])
+         (into []))))
---
+++ src/test/app_macros/props_test.cljs
@@ -0,0 +1,110 @@
+(ns app-macros.props-test
+  (:require [cljs.test :refer-macros [deftest is]]
+            [app-macros.props :as p]))
+
+;;;; Padding
+
+(deftest pad-by-equality
+  (is (= (into [] (p/pad-by = nil [1 1 2 3 4 4 5]))
+         [1 nil 1 2 3 4 nil 4 5])))
+
+;;;; Value testing
+
+(deftest values
+  (and (is (p/value? 5))
+       (is (p/value? 10))
+       (is (p/value? "Hello"))
+       (is (p/value? "World"))
+       (is (p/value? :foo))
+       (is (p/value? :bar))
+       (is (p/value? '_))))
+
+;;;; Property types
+
+(deftest property-types
+  (and (is (= :property (p/property-type 'foo)))
+       (is (= :property (p/property-type 'bar)))
+       (is (= :link     (p/property-type '[foo 5])))
+       (is (= :link     (p/property-type '[bar "Hello"])))
+       (is (= :link     (p/property-type '[baz :hello])))
+       (is (= :join     (p/property-type '{foo ...})))
+       (is (= :join     (p/property-type '{bar Hello})))))
+
+;;;; Parsing
+
+(deftest parse-one-prop
+  (and (is (= (p/parse '[foo]) '[{:name foo :type :property}]))
+       (is (= (p/parse '[bar]) '[{:name bar :type :property}]))))
+
+(deftest parse-two-props
+  (is (= (p/parse '[foo bar])
+         '[{:name foo :type :property}
+           {:name bar :type :property}])))
+
+(deftest parse-one-ident
+  (and (is (= (p/parse '[[user 5]])
+              '[{:name user :type :link :target 5}]))
+       (is (= (p/parse '[[user "foo"]])
+              '[{:name user :type :link :target "foo"}]))
+       (is (= (p/parse '[[user :foo]])
+              '[{:name user :type :link :target :foo}]))))
+
+(deftest parse-two-idents
+  (and (is (= (p/parse '[[user 5] [post 10]])
+              '[{:name user :type :link :target 5}
+                {:name post :type :link :target 10}]))))
+
+(deftest parse-one-join
+  (and (is (= (p/parse '[{foo ...}])
+              '[{:name foo :type :join :target ...}]))
+       (is (= (p/parse '[{bar User}])
+              '[{:name bar :type :join :target User}]))
+       (is (= (p/parse '[{baz 5}])
+              '[{:name baz :type :join :target 5}]))))
+
+(deftest parse-three-joins
+  (and (is (= (p/parse '[{foo ...}
+                         {bar User}
+                         {baz 5}])
+              '[{:name foo :type :join :target ...}
+                {:name bar :type :join :target User}
+                {:name baz :type :join :target 5}]))))
+
+(deftest parse-basic-child-props
+  (and (is (= (p/parse '[foo [bar]])
+              '[{:name foo/bar :type :property}]))
+       (is (= (p/parse '[foo [bar baz]])
+              '[{:name foo/bar :type :property}
+                {:name foo/baz :type :property}]))
+       (is (= (p/parse '[foo [bar] baz [ruux]])
+              '[{:name foo/bar :type :property}
+                {:name baz/ruux :type :property}]))
+       (is (= (p/parse '[foo bar [baz] ruux])
+              '[{:name foo :type :property}
+                {:name bar/baz :type :property}
+                {:name ruux :type :property}]))))
+
+(deftest parse-join-child-props
+  (and (is (= (p/parse '[foo [{bar Baz}]])
+              '[{:name foo/bar :type :join :target Baz}]))
+       (is    (= (p/parse '[foo [{bar Baz} {baz Ruux}]])
+                 '[{:name foo/bar :type :join :target Baz}
+                   {:name foo/baz :type :join :target Ruux}]))
+       (is (= (p/parse '[foo [{bar ...}]])
+              '[{:name foo/bar :type :join :target ...}]))
+       (is (= (p/parse '[foo [{bar 5}]])
+              '[{:name foo/bar :type :join :target 5}]))))
+
+(deftest parse-link-child-props
+  (and (is (= (p/parse '[foo [[bar _]]])
+              '[{:name foo/bar :type :link :target _}]))
+       (is (= (p/parse '[foo [[bar 15]]])
+              '[{:name foo/bar :type :link :target 15}]))
+       (is (= (p/parse '[foo [[bar :baz]]])
+              '[{:name foo/bar :type :link :target :baz}]))
+       (is (= (p/parse '[foo [[bar _] [baz 5]]])
+              '[{:name foo/bar :type :link :target _}
+                {:name foo/baz :type :link :target 5}]))
+       (is (= (p/parse '[foo [[bar _]] baz [[ruux 15]]])
+              '[{:name foo/bar :type :link :target _}
+                {:name baz/ruux :type :link :target 15}]))))
