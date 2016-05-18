380fd644a381635106bce1932d5c738b7ee5bce2
Jannis Pohlmann <contact@jannispohlmann.de>
2016-05-18 13:07:15 +0200

    Add defview configuration options and wrapper view option for render
    This wrapper view allows to define render functions that return
    multiple child expressions. Since React requires render to return
    a single element, defview will now automatically wrap multiple child
    expressions in a wrapper view (defaulting to om.dom/div). So
    
        (render [this]
          (foo)
          (bar))
    
    will be turned into
    
        (render [this]
          ((workflo.macros.wrapper) (om/props this)
            (foo)
            (bar)))
    
    This commits also adds a basic code generation test for this.

--- src/main/workflo/macros/view.cljs
+++ src/main/workflo/macros/view.cljs
@@ -1,3 +1,36 @@
 (ns workflo.macros.view
   (:require [om.next]
             [om.dom]))
+
+;;;; Configuration
+
+(def +configuration+
+  "Configuration options for the defview macro."
+  (atom {:wrapper-view nil}))
+
+(defn configure!
+  "Configures the defview macro, usually before it is being used.
+   Supports the following options:
+
+   :wrapper-view - a React element factory to use for wrapping
+                   the body of render functions if render has
+                   more than a single child expression."
+  [{:keys [wrapper-view] :as options}]
+  (swap! +configuration+ assoc
+         :wrapper-view wrapper-view))
+
+(defn get-config
+  "Returns the configuration for a given configuration key, e.g.
+   :wrapper-view."
+  [key]
+  (@+configuration+ key))
+
+(defn wrapper
+  "Returns a wrapper factory for use in render functions. If no
+   wrapper function is defined, issues a warning and returns
+   om.dom/div to avoid breaking apps entirely."
+  []
+  (if-not (get-config :wrapper-view)
+    (do (js/console.warning "No wrapper view defined for defview.")
+        om.dom/div)
+    (get-config :wrapper-view)))
