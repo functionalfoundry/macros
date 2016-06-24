6baba074fdd58523e7eec29ef8ef982f860f5fbb
Jannis Pohlmann <contact@jannispohlmann.de>
2016-06-24 20:25:30 +0200

    WIP

--- src/main/workflo/macros/command.clj
+++ src/main/workflo/macros/command.clj
@@ -1,6 +1,8 @@
 (ns workflo.macros.command
   (:require [clojure.spec :as s]
-            [workflo.macros.specs.query]))
+            [clojure.spec.gen :as gen]
+            [workflo.macros.specs.query]
+            [workflo.macros.command.util :as util]))
 
 (s/def ::command-name
   symbol?)
@@ -18,8 +20,7 @@
               (s/spec map?)})))
 
 (s/def ::command-inputs
-  (s/tuple (s/? ::command-query)
-           (s/? ::command-data-spec)))
+  (s/tuple (s/? ::command-query) ::command-data-spec))
 
 (s/def ::command-form
   seq?)
@@ -27,20 +28,39 @@
 (s/def ::command-implementation
   seq?)
 
+(s/def ::defcommand-args
+  (s/cat :name ::command-name
+         :forms
+         (s/spec (s/cat :description (s/? ::command-description)
+                        :inputs ::command-inputs
+                        :forms (s/* ::command-form)
+                        :implementation ::command-implementation))))
+
+(s/fdef defcommand*
+  :args ::defcommand-args
+  :ret  ::s/any)
+
 (defn defcommand*
   ([name forms]
    (defcommand* name forms nil))
   ([name forms env]
-   (let [])))
-
-(s/fdef defcommand
-  :args (s/cat :name ::command-name
-               :description (s/? ::command-description)
-               :inputs (s/? ::command-inputs)
-               :forms (s/* ::command-form)
-               :implementation ::command-implementation)
-  :ret  ::s/any)
+   (let [args        (s/conform ::defcommand-args [name forms])
+         description (:description (:forms args))
+         inputs      (:inputs (:forms args))
+         cmd-forms   (:forms (:forms args))
+         cmd-impl    (:implementation (:forms args))
+         cache-query (when #(= 2 (count inputs))
+                       (first inputs))
+         data-spec   (last inputs)
+         name-sym    (util/unqualify name)]
+     (println "NAME" name name-sym)
+     (println "DESCRIPTION" description)
+     (println "CACHE-QUERY" cache-query)
+     (println "DATA-SPEC" data-spec)
+     (println "CMD-FORMS" cmd-forms)
+     (println "CMD-IMPL" cmd-impl))))
 
 (defmacro defcommand
-  [name & forms]
-  (defcommand* name forms &env))
+  [name]
+  (println "NAME" name)
+  #_(defcommand* name forms &env))
