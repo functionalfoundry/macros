c546d688b51623b41f5f424f92e063a7945ea53b
Jannis Pohlmann <contact@jannispohlmann.de>
2016-07-27 16:25:34 +0200

    Use defconfig for the defscreen macro configuration

--- src/main/workflo/macros/screen.cljs
+++ src/main/workflo/macros/screen.cljs
@@ -1,24 +1,11 @@
 (ns workflo.macros.screen
-  (:require [workflo.macros.registry :refer-macros [defregistry]]
+  (:require [workflo.macros.config :refer-macros [defconfig]]
+            [workflo.macros.registry :refer-macros [defregistry]]
             [workflo.macros.util.js :refer [resolve]]))
 
 ;;;; Configuration options for the defscreen macro
 
-(defonce +configuration+
-  (atom {}))
-
-(defn configure!
-  "Configures the defscreen macro, usually before it is being used.
-   Supports the following options:
-
-   tbd."
-  [{:keys [] :as options}]
-  (swap! +configuration+ assoc))
-
-(defn get-config
-  "Returns the configuration for a given configuration key."
-  [key]
-  (@+configuration+ key))
+(defconfig screen {})
 
 ;;;; Screen registry
 
