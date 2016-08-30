24affb2f687be1b1b43524fc7a2e80bd6053427a
Jannis Pohlmann <contact@jannispohlmann.de>
2016-08-30 13:22:26 +0200

    Fix entity tests to include name and use spec instead of schema

--- src/test/workflo/macros/entity_test.clj
+++ src/test/workflo/macros/entity_test.clj
@@ -5,64 +5,34 @@
 
 (deftest minimal-defentity
   (is (= '(do
-            (def macros-user-schema map?)
+            (def macros-user-name 'macros/user)
+            (def macros-user-spec map?)
             (def macros-user-definition
-              {:schema pod/macros-user-schema}))
+              {:name pod/macros-user-name
+               :spec pod/macros-user-spec})
+            (workflo.macros.entity/register-entity!
+             'macros/user pod/macros-user-definition))
          (macroexpand-1 `(defentity macros/user
-                           (~'schema ~'map?))))))
-
-(deftest defentity-with-validation
-  (is (= '(do
-            (def macros-user-validation
-              (s/keys :req [:foo/bar]
-                      :opt [:bar/baz]))
-            (def macros-user-schema map?)
-            (def macros-user-definition
-              {:schema pod/macros-user-schema
-               :validation pod/macros-user-validation}))
-         (macroexpand-1 `(defentity macros/user
-                           (~'validation
-                             (~'s/keys :req [:foo/bar]
-                                       :opt [:bar/baz]))
-                           (~'schema ~'map?))))))
+                           (~'spec ~'map?))))))
 
 (deftest defentity-with-auth
   (is (= '(do
+            (def macros-user-name 'macros/user)
             (defn macros-user-auth
               [{:keys [foo bar]}]
               (println foo bar))
             (def macros-user-auth-query
               '[{:name foo :type :property}
                 {:name bar :type :property}])
-            (def macros-user-schema map?)
+            (def macros-user-spec map?)
             (def macros-user-definition
-              {:auth pod/macros-user-auth
+              {:name pod/macros-user-name
+               :auth pod/macros-user-auth
                :auth-query pod/macros-user-auth-query
-               :schema pod/macros-user-schema}))
+               :spec pod/macros-user-spec})
+            (workflo.macros.entity/register-entity!
+             'macros/user pod/macros-user-definition))
          (macroexpand-1 `(defentity macros/user
                            ~'(auth [foo bar]
                                (println foo bar))
-                           ~'(schema map?))))))
-
-(deftest defentity-with-auth-and-validation
-  (is (= '(do
-            (defn macros-user-auth
-              [{:keys [foo bar]}]
-              (println foo bar))
-            (def macros-user-auth-query
-              '[{:name foo :type :property}
-                {:name bar :type :property}])
-            (def macros-user-validation
-              (s/keys :req-un [::foo ::bar]))
-            (def macros-user-schema map?)
-            (def macros-user-definition
-              {:auth pod/macros-user-auth
-               :auth-query pod/macros-user-auth-query
-               :validation pod/macros-user-validation
-               :schema pod/macros-user-schema}))
-         (macroexpand-1 `(defentity macros/user
-                           ~'(auth [foo bar]
-                               (println foo bar))
-                           ~'(validation
-                               (s/keys :req-un [::foo ::bar]))
-                           ~'(schema map?))))))
+                           ~'(spec map?))))))
