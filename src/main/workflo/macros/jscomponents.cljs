77dfbea421367eabce41d40cf9f33a33e1860d4e
Jannis Pohlmann <contact@jannispohlmann.de>
2016-10-16 13:54:25 +0200

    Fix camelize-keys alias in ClojureScript

--- src/main/workflo/macros/jscomponents.cljs
+++ src/main/workflo/macros/jscomponents.cljs
@@ -1,1 +1,6 @@
-(ns workflo.macros.jscomponents)
+(ns workflo.macros.jscomponents
+  (:require-macros [workflo.macros.jscomponents
+                    :refer [defjscomponents]])
+  (:require [workflo.macros.util.string]))
+
+(def camelize-keys workflo.macros.util.string/camelize-keys)
