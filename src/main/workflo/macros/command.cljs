442e1e7bb5f4dd36aea1a6467f17655ce10f0590
Jannis Pohlmann <contact@jannispohlmann.de>
2017-01-04 19:31:51 +0100

    Add support for auth and auth-query forms in defcommand

--- src/main/workflo/macros/command.cljs
+++ src/main/workflo/macros/command.cljs
@@ -4,7 +4,8 @@
             [workflo.macros.bind]
             [workflo.macros.config :refer [defconfig]]
             [workflo.macros.query :as q]
-            [workflo.macros.registry :refer [defregistry]]))
+            [workflo.macros.registry :refer [defregistry]]
+            [workflo.macros.specs.query :as specs.query]))
 
 ;;;; Configuration options for the defcommand macro
 
@@ -16,6 +17,11 @@
   ;;          this function is used to query a cache for data that
   ;;          the command being executed needs to run.
   ;;
+  ;; :auth-query - a function that takes a parsed auth query and the context
+  ;;               that was passed to `run-command!` by the caller;
+  ;;               this function is used to query a cache for data that
+  ;;               is needed to authorize the command execution.
+  ;;
   ;; :process-emit - a function that is called after a command has
   ;;                 been executed; it takes the data returned from
   ;;                 the command emit function as well as the context
@@ -23,11 +29,26 @@
   ;;                 and processes or transforms the command output in
   ;;                 whatever way is desirable.
   {:query nil
+   :auth-query nil
    :process-emit nil})
 
 ;;;; Command registry
 
 (defregistry command)
+
+;;;; Query utilities
+
+(defn valid-query?
+  [query]
+  (s/valid? ::specs.query/query query))
+
+(defn conform-and-parse
+  [query]
+  (q/conform-and-parse query))
+
+(defn bind-query-parameters
+  [query & data-maps]
+  (q/bind-query-parameters query (apply merge data-maps)))
 
 ;;;; Command execution
 
@@ -40,13 +61,25 @@
        (assert (s/valid? (:spec definition) data)
                (str "Command data is invalid:"
                     (s/explain-str (:spec definition) data))))
-     (let [query          (some-> definition :query
-                                  (q/bind-query-parameters data))
-           query-result   (when query
-                            (some-> (get-command-config :query)
-                                    (apply [query context])))
-           command-result ((:emit definition) query-result data)]
-       (if (get-command-config :process-emit)
-         (-> (get-command-config :process-emit)
-             (apply [command-result context]))
-         command-result)))))
+     (let [bind-data         (merge context data)
+           query-result      (when-let [query-hook (get-command-config :query)]
+                               (some-> (:query definition)
+                                       (q/bind-query-parameters bind-data)
+                                       (query-hook context)))
+           auth-query-result (when-let [query-hook (get-command-config :auth-query)]
+                               (some-> (:auth-query definition)
+                                       (q/bind-query-parameters bind-data)
+                                       (query-hook context)))
+           authorized?       (if-let [auth-fn (:auth definition)]
+                               (auth-fn query-result auth-query-result)
+                               true)]
+       (if authorized?
+         (let [emit-output ((:emit definition) query-result data)]
+           (if (get-command-config :process-emit)
+             (-> (get-command-config :process-emit)
+                 (apply [emit-output context]))
+             emit-output))
+         (throw (ex-info (str "Not authorized to run command: " cmd-name)
+                         {:cmd-name cmd-name
+                          :data data
+                          :context context})))))))
--- src/test/workflo/macros/command_test.clj
+++ src/test/workflo/macros/command_test.clj
@@ -56,8 +56,8 @@
             (workflo.macros.command/register-command!
              'user/update pod/user-update-definition))
          (macroexpand-1 `(defcommand user/update
-                           (~'query ~'[user [name email]])
                            (~'spec ~'vector?)
+                           (~'query ~'[user [name email]])
                            (~'emit {:some :data}))))))
 
 (deftest defcommand-with-forms
@@ -107,39 +107,205 @@
             (workflo.macros.command/register-command!
              'user/create pod/user-create-definition))
          (macroexpand-1 `(defcommand user/create
-                           (~'query ~'[db [id]])
                            (~'spec ~'map?)
+                           (~'query ~'[db [id]])
                            (~'foo [:bar])
+                           (~'emit :result))))))
+
+(deftest defcommand-with-query-and-auth
+  (is (= '(do
+            (defn user-update-emit
+              [query-result data]
+              (workflo.macros.bind/with-query-bindings
+                [{:name db/id :type :property}]
+                query-result
+                :result))
+            (def user-update-query
+              '[{:name db/id :type :property}])
+            (defn user-update-auth
+              [query-result auth-query-result]
+              (workflo.macros.bind/with-query-bindings
+                [{:name db/id :type :property}]
+                query-result
+                :foo))
+            (def user-update-definition
+              {:emit pod/user-update-emit
+               :query pod/user-update-query
+               :auth pod/user-update-auth})
+            (workflo.macros.command/register-command!
+             'user/update pod/user-update-definition))
+         (macroexpand-1 `(defcommand user/update
+                           (~'query ~'[db [id]])
+                           (~'auth :foo)
+                           (~'emit :result))))))
+
+(deftest defcommand-with-query-and-auth-and-auth-query
+  (is (= '(do
+            (defn user-update-emit
+              [query-result data]
+              (workflo.macros.bind/with-query-bindings
+                [{:name db/id :type :property}]
+                query-result
+                :result))
+            (def user-update-query
+              '[{:name db/id :type :property}])
+            (def user-update-auth-query
+              '[{:name foo/bar :type :property}])
+            (defn user-update-auth
+              [query-result auth-query-result]
+              (workflo.macros.bind/with-query-bindings
+                [{:name db/id :type :property}]
+                query-result
+                (workflo.macros.bind/with-query-bindings
+                  [{:name foo/bar :type :property}]
+                  auth-query-result
+                  :foo)))
+            (def user-update-definition
+              {:emit pod/user-update-emit
+               :query pod/user-update-query
+               :auth-query pod/user-update-auth-query
+               :auth pod/user-update-auth})
+            (workflo.macros.command/register-command!
+             'user/update pod/user-update-definition))
+         (macroexpand-1 `(defcommand user/update
+                           (~'query ~'[db [id]])
+                           (~'auth-query ~'[foo [bar]])
+                           (~'auth :foo)
+                           (~'emit :result))))))
+
+(deftest defcommand-with-query-and-auth-and-generated-auth-query
+  (is (= '(do
+            (defn user-update-emit
+              [query-result data]
+              (workflo.macros.bind/with-query-bindings
+                [{:name db/id :type :property}]
+                query-result
+                :result))
+            (def user-update-query
+              '[{:name db/id :type :property}])
+            (def user-update-auth-query
+              (workflo.macros.command/conform-and-parse (gen-query)))
+            (defn user-update-auth
+              [query-result auth-query-result]
+              (workflo.macros.bind/with-query-bindings
+                [{:name db/id :type :property}]
+                query-result
+                (workflo.macros.bind/with-query-bindings
+                  (workflo.macros.command/conform-and-parse (gen-query))
+                  auth-query-result
+                  :foo)))
+            (def user-update-definition
+              {:emit pod/user-update-emit
+               :query pod/user-update-query
+               :auth-query pod/user-update-auth-query
+               :auth pod/user-update-auth})
+            (workflo.macros.command/register-command!
+             'user/update pod/user-update-definition))
+         (macroexpand-1 `(defcommand user/update
+                           (~'query ~'[db [id]])
+                           (~'auth-query ~'(gen-query))
+                           (~'auth :foo)
                            (~'emit :result))))))
 
 ;;;; Exercise run-command!
 
-;;; Define a spec for the command data
+(deftest exercise-run-command-with-authorization
+  ;; Use a user-counters atom for testing
+  (def user-counters
+    (atom [{:user/name :jeff :user/counter 0}
+           {:user/name :joe :user/counter 0}]))
 
-(s/def ::user-name string?)
-(s/def ::user-email string?)
-(s/def ::user-create-data
-  (s/keys :req-un [::user-name ::user-email]))
+  ;; Example query, auth-query and process-emit hooks
+  (defn example-query-hook
+    [query context]
+    ;; Verify the query is what we'd expect
+    (is (= [{:name 'user
+             :type :join
+             :join-source {:name 'user :type :property}
+             :join-target [{:name 'user/name :type :property :alias 'user-name}
+                           {:name 'user/counter :type :property}]
+             :parameters {'user/name (:user context)}}]
+           query))
+    ;; Simulate running the query
+    {:user (first (filter (fn [user-counter]
+                            (= (:user/name user-counter)
+                               (:user context)))
+                          @user-counters))})
 
-;;; Define example query and run emits
+  (defn example-auth-query-hook
+    [query context]
+    ;; Verify the auth query is what we'd expect
+    (is (= '[{:name permissions
+              :type :join
+              :join-source {:name permissions :type :property}
+              :join-target [{:name name :type :property}]}]
+           query))
+    ;; Simulate running the auth query, returning permissions only for jeff
+    (if (= :jeff (:user context))
+      {:permissions [{:name :update}]}
+      {:permissions []}))
 
-(defn example-query
-  [query _]
-  {:db/id 15})
+  (defn example-process-emit-hook
+    [emit-output context]
+    (swap! user-counters
+           (fn [user-counters]
+             (mapv (fn [user-counter]
+                     (cond-> user-counter
+                       (= (:user/name emit-output)
+                          (:user/name user-counter))
+                       (assoc :user/counter (:user/counter emit-output))))
+                   user-counters))))
 
-(defn example-process-emit
-  [data _]
-  data)
 
-(defcommand user/create
-  (query [db [id]])
-  (spec ::user-create-data)
-  (emit
-   {:cache {:db-id id}}))
+  (c/configure-commands!
+   {:query example-query-hook
+    :auth-query example-auth-query-hook
+    :process-emit example-process-emit-hook})
 
-(c/configure-commands!
-  {:query example-query
-   :process-emit example-process-emit})
+  ;; Define a spec for the command data
+  (s/def :update-user-counter/user keyword?)
+  (s/def ::update-user-counter-data
+    (s/keys :req [:update-user-counter/user]))
 
-(c/run-command! 'user/create {:user-name "Jeff"
-                              :user-email "jeff@jeff.org"})
+  (defcommand update-user-counter
+    (spec ::update-user-counter-data)
+    (query
+      [({user [user [name :as user-name counter]]}
+        {user/name ?user})])
+    (auth-query
+      [{permissions [name]}])
+    (auth
+      (is (or (= {:user/name :jeff :user/counter 0} user)
+              (= {:user/name :joe :user/counter 0} user)))
+      (if (= :jeff user-name)
+        (is (= [{:name :update}] permissions))
+        (is (= [] permissions)))
+      ;; Only users for which permissions are available are authorized
+      ;; to update counters
+      (and user (seq permissions)))
+    (emit
+      {:user/name user-name
+       :user/counter (inc counter)}))
+
+  ;; Run the command as an authorized user
+  (c/run-command! 'update-user-counter
+                  {:update-user-counter/user :jeff}
+                  {:user :jeff})
+
+  ;; Verify that the user's counter has changed
+  (is (= [{:user/name :jeff :user/counter 1}
+          {:user/name :joe :user/counter 0}]
+         @user-counters))
+
+  ;; Verify that running the command as an unauthorized user
+  ;; throws an exception
+  (is (thrown? Exception
+               (c/run-command! 'update-user-counter
+                               {:update-user-counter/user :joe}
+                               {:user :joe})))
+
+  ;; Verify that after the exception, the data hasn't changed,
+  ;; meaning that the command wasn't executed
+  (is (= [{:user/name :jeff :user/counter 1}
+          {:user/name :joe :user/counter 0}]
+         @user-counters)))
