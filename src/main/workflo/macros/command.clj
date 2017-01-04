0fd52b41189ea89fd30fb8b5876a4061653c0190
Jannis Pohlmann <contact@jannispohlmann.de>
2017-01-04 19:31:51 +0100

    WIP: Add support for auth and auth-query forms in defcommand
    TODO:
    
    * Only include those forms in the resulting command if we're
      generating for Clojure, not for ClojureScript; this is assuming
      that the ClojureScript side doesn't need to do authorization -
      in that case we wouldn't want server-side authorization logic
      to be visible in clients; plus if it isn't used, we can omit it
      to not add to the JS code size

--- src/main/workflo/macros/command.clj
+++ src/main/workflo/macros/command.clj
@@ -5,6 +5,7 @@
             [workflo.macros.query :as q]
             [workflo.macros.config :refer [defconfig]]
             [workflo.macros.registry :refer [defregistry]]
+            [workflo.macros.specs.query :as specs.query]
             [workflo.macros.specs.command]
             [workflo.macros.util.form :as f]
             [workflo.macros.util.symbol :refer [unqualify]]))
@@ -19,6 +20,11 @@
   ;;          this function is used to query a cache for data that
   ;;          the command being executed needs to run.
   ;;
+  ;; :auth-query - a function that takes a parsed auth query and the context
+  ;;               that was passed to `run-command!` by the caller;
+  ;;               this function is used to query a cache for data that
+  ;;               is needed to authorize the command execution.
+  ;;
   ;; :process-emit - a function that is called after a command has
   ;;                 been executed; it takes the data returned from
   ;;                 the command emit function as well as the context
@@ -26,11 +32,26 @@
   ;;                 and processes or transforms the command output in
   ;;                 whatever way is desirable.
   {:query nil
+   :auth-query nil
    :process-emit nil})
 
 ;;;; Command registry
 
 (defregistry command)
+
+;;;; Query utilities
+
+(defn valid-query?
+  [query]
+  (s/valid? ::specs.query/query query))
+
+(defn conform-and-parse
+  [query]
+  (q/conform-and-parse query))
+
+(defn bind-query-parameters
+  [query & data-maps]
+  (q/bind-query-parameters query (apply merge data-maps)))
 
 ;;;; Command execution
 
@@ -43,16 +64,28 @@
        (assert (s/valid? (:spec definition) data)
                (str "Command data is invalid:"
                     (s/explain-str (:spec definition) data))))
-     (let [query          (some-> definition :query
-                                  (q/bind-query-parameters data))
-           query-result   (when query
-                            (some-> (get-command-config :query)
-                                    (apply [query context])))
-           command-result ((:emit definition) query-result data)]
-       (if (get-command-config :process-emit)
-         (-> (get-command-config :process-emit)
-             (apply [command-result context]))
-         command-result)))))
+     (let [bind-data         (merge context data)
+           query-result      (when-let [query-hook (get-command-config :query)]
+                               (some-> (:query definition)
+                                       (q/bind-query-parameters bind-data)
+                                       (query-hook context)))
+           auth-query-result (when-let [query-hook (get-command-config :auth-query)]
+                               (some-> (:auth-query definition)
+                                       (q/bind-query-parameters bind-data)
+                                       (query-hook context)))
+           authorized?       (if-let [auth-fn (:auth definition)]
+                               (auth-fn query-result auth-query-result)
+                               true)]
+       (if authorized?
+         (let [emit-output ((:emit definition) query-result data)]
+           (if (get-command-config :process-emit)
+             (-> (get-command-config :process-emit)
+                 (apply [emit-output context]))
+             emit-output))
+         (throw (ex-info (str "Not authorized to run command: " cmd-name)
+                         {:cmd-name cmd-name
+                          :data data
+                          :context context})))))))
 
 ;;;; The defcommand macro
 
@@ -64,35 +97,42 @@
   ([name forms]
    (defcommand* name forms nil))
   ([name forms env]
-   (let [args-spec   :workflo.macros.specs.command/defcommand-args
-         args        (if (s/valid? args-spec [name forms])
-                       (s/conform args-spec [name forms])
-                       (throw (Exception.
-                               (s/explain-str args-spec
-                                              [name forms]))))
-         description (:description (:forms args))
-         query       (some-> args :forms :query :form-body q/parse)
-         spec        (some-> args :forms :spec :form-body)
-         name-sym    (unqualify name)
-         forms       (cond-> (:forms (:forms args))
-                       true        (conj (:emit (:forms args)))
-                       description (conj {:form-name 'description})
-                       query       (conj {:form-name 'query})
-                       spec        (conj {:form-name 'spec}))
-         form-fns    (->> forms
-                          (remove (comp nil? :form-body))
-                          (map #(update % :form-name
-                                        f/prefixed-form-name
-                                        name-sym))
-                          (map #(assoc % :form-args
-                                       '[query-result data]))
-                          (map #(cond-> %
-                                  (not (empty? query))
-                                  (update :form-body
-                                          util/wrap-with-query-bindings
-                                          query)))
-                          (map f/form->defn))
-         def-sym     (f/qualified-form-name 'definition name-sym)]
+   (let [args-spec         :workflo.macros.specs.command/defcommand-args
+         args              (if (s/valid? args-spec [name forms])
+                             (s/conform args-spec [name forms])
+                             (throw (Exception.
+                                     (s/explain-str args-spec
+                                                    [name forms]))))
+         description       (:description (:forms args))
+         spec              (some-> args :forms :spec :form-body)
+         query             (some-> args :forms :query :form-body q/parse)
+         auth-query        (some-> args :forms :auth-query :form-body)
+         parsed-auth-query (if (vector? auth-query)
+                             (q/conform-and-parse auth-query)
+                             `(workflo.macros.command/conform-and-parse ~auth-query))
+         auth              (some-> args :forms :auth :form-body)
+         name-sym          (unqualify name)
+         forms             (cond-> (:forms (:forms args))
+                             true        (conj (:emit (:forms args)))
+                             description (conj {:form-name 'description})
+                             spec        (conj {:form-name 'spec})
+                             query       (conj {:form-name 'query})
+                             auth-query  (conj {:form-name 'auth-query})
+                             auth        (conj {:form-name 'auth}))
+         form-fns          (->> forms
+                                (remove (comp nil? :form-body))
+                                (map #(update % :form-name
+                                              f/prefixed-form-name
+                                              name-sym))
+                                (map #(assoc % :form-args
+                                             '[query-result data]))
+                                (map #(cond-> %
+                                        (not (empty? query))
+                                        (update :form-body
+                                                util/wrap-with-query-bindings
+                                                query)))
+                                (map f/form->defn))
+         def-sym           (f/qualified-form-name 'definition name-sym)]
      `(do
         ~@form-fns
         ~@(when description
@@ -100,6 +140,31 @@
         ~@(when query
             `((~'def ~(f/prefixed-form-name 'query name-sym)
                '~query)))
+        ~@(when auth-query
+            `((~'def ~(f/prefixed-form-name 'auth-query name-sym)
+               ~(if (vector? auth-query)
+                  `'~parsed-auth-query
+                  parsed-auth-query))))
+        ~@(when auth
+            `(~(f/make-defn name-sym 'auth
+                 '[query-result auth-query-result]
+                 [(cond
+                    (and query auth-query)
+                    `(workflo.macros.bind/with-query-bindings
+                       ~query ~'query-result
+                       (workflo.macros.bind/with-query-bindings
+                         ~parsed-auth-query ~'auth-query-result
+                         ~@auth))
+
+                    auth-query
+                    `(workflo.macros.bind/with-query-bindings
+                       ~parsed-auth-query ~'auth-query-result
+                       ~@auth)
+
+                    query
+                    `(workflo.macros.bind/with-query-bindings
+                       ~query ~'query-result
+                       ~@auth))])))
         ~@(when spec
             `(~(f/make-def name-sym 'spec spec)))
         ~(f/make-def name-sym 'definition
